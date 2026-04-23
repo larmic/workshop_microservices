6daea1d3282944ec6d5eaeca907a7dd1e183182d
larmic <mail@larmic.de>
2026-04-23 19:03:57 +0200

    -

--- services/shared/handler/openapi.go
+++ services/shared/handler/openapi.go
@@ -8,7 +8,7 @@ import (
 func OpenapiHandler(spec []byte) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
-		w.Header().Set("Content-Type", "application/yaml")
+		w.Header().Set("Content-Type", "text/yaml; charset=utf-8")
 		w.Write(spec)
 	}
 }
